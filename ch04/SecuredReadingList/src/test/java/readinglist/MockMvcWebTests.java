a0f6532b24ae248500e73e55422abf4569bdf751
bob zhou <bob.zhou@dbschenker.com>
2018-02-06 15:37:32 +0800

    change the gradle to make build success change the deprecated annotation

--- ch04/SecuredReadingList/src/test/java/readinglist/MockMvcWebTests.java
+++ ch04/SecuredReadingList/src/test/java/readinglist/MockMvcWebTests.java
@@ -9,7 +9,8 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+//import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -22,7 +23,8 @@ import readinglist.ReadingListApplication;
 import readinglist.Reader;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = ReadingListApplication.class)
+//@SpringApplicationConfiguration(classes = ReadingListApplication.class)
+@SpringBootTest(classes = ReadingListApplication.class)
 @WebAppConfiguration
 public class MockMvcWebTests {
 
